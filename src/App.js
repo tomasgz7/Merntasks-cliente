2cbe9808cd376536030d74f82523cb73a1b87a55
tomasgz7 <tomasguzmancabj@hotmail.com>
2022-03-20 13:42:43 -0300

    3 Rutas principales.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { 
+  BrowserRouter,
+  Routes,
+  Route,
+     } from 'react-router-dom';
+import Login from './components/auth/Login';
+import NuevaCuenta from './components/auth/NuevaCuenta';
+import Proyectos from './components/proyectos/Proyectos';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route>
+        <Route path='/' element={ <Login/> } />
+        <Route path='/nueva-cuenta' element={ <NuevaCuenta/> } />
+        <Route path='/proyectos' element={ <Proyectos/> } />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
